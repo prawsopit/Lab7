e558de1188d3ee80a8fa524b286494d7ce405f09
prawsopit <prawsopit@gmail.com>
2022-12-13 17:27:33 +0700

    lab7_2.cpp

--- lab7_2.cpp
+++ lab7_2.cpp
@@ -8,7 +8,25 @@ int main(){
 	while(i < 5000){
 		P[i] = 2*i+1;
 		i++;
+	} 
+	cout << "Please input k: ";
+	cin >> k;
+	if (k>0){
+		i = 0;
+		result = 0;
+		while(i<5000){
+			if(P[i]%k==0){
+				result = result-P[i];
+			}else{
+				result = result+P[i];
+			}
+			i=i+1;
+		}
+		cout << "Result = " << result;
+	}else{
+		cout << "Invalid input!!!";
 	}
 	
 	return 0;
 }
+
--- lab7_3.cpp
+++ lab7_3.cpp
@@ -1,15 +1,25 @@
 #include<iostream>
-
+#include<cmath>
 using namespace std;
 
+int adiff(int A,int B){  
+int C ;
+  C=abs(A -B) ;
+  while(C >= 360){
+     C=C-360 ;
+  }
+  if(C >=180)
+   C= 360-C;
+  return C ;
 
+}
 int main(){
-  cout << adiff(180,270);
-  cout << adiff(210,45);
-  cout << adiff(0,360);
-  cout << adiff(10,350);
-  cout << adiff(95,260);
-  cout << adiff(90,-90);
-  cout << adiff(1000,280);
-  cout << adiff(60,244);
+  cout << adiff(180,270) << "\n";
+  cout << adiff(210,45)<< "\n" ;
+  cout << adiff(0,360) << "\n";
+  cout << adiff(10,350) << "\n";
+  cout << adiff(95,260) << "\n";
+  cout << adiff(90,-90) << "\n";
+  cout << adiff(1000,280) << "\n";
+  cout << adiff(60,244)<< "\n" ;
 }
